8b1faa21e8e25010bfd022c8c487d754e2da36f7
hemantsahu25 <hemantsahu8115@gmail.com>
2025-08-29 14:01:47 +0530

    day 8

--- Day 8/index.js
+++ Day 8/index.js
@@ -7,3 +7,23 @@
 
  You may assume each function in the array accepts one integer as input and returns one integer as output.
  */
+
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+    return function(x) {
+        for (let i = functions.length-1; i>=0; i--)
+        {
+            x=functions[i](x)
+        }
+        return x;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
